092b8f77e1157ff3edbb0d28e67680c259a1eb73
Todd Kushner <kushnertodd@gmail.com>
2026-09-09 11:46:17 -0600

    cleanup

--- bin/run-jupyter.sh
+++
@@ -1,5 +0,0 @@
-jupyter-notebook \
-	--no-browser \
-	--port=8000 \
-	python_intro.ipynb 
-
--- bin/unprotect.sh
+++
@@ -1,9 +0,0 @@
-cd /home/kushnertodd_toddkushnerllc_com/CFTM-intro-to-python
-chmod 755 . \
-	notebooks \
-	bin
-chmod 644 \
-	LICENSE \
-	README.md \
-	python_intro.ipynb \
-	notebooks/*
