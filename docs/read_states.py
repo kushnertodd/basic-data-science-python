772db0a5350e2f97eb6fa32ca19161358e1e8d2a
Todd Kushner <kushnertodd@gmail.com>
2026-09-09 11:40:05 -0600

    cleanup

--- docs/read_states.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://gist.githubusercontent.com/kushnertodd/4001ed7df2be5d09c95e7df11a4afcf7/raw/0b4fab8d35dbc1d1f380959f12eb43f25d2674b4/states.csv"
-response = requests.get(url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.text
-    print(data)
