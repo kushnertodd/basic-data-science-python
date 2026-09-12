5accf7de9d6f24282e1301080ebd47c3adcb5c13
Todd Kushner <kushnertodd@gmail.com>
2026-09-12 11:19:16 -0600

    regions_stats

---
+++ data/States/regions_stats.py
@@ -0,0 +1,8 @@
+region_stats = {
+    "New England":  {"population_2020": 0, "sq_miles": 0, "sq_km": 0, "reps": 0},
+    "Southwest":    {"population_2020": 0, "sq_miles": 0, "sq_km": 0, "reps": 0},
+    "Mid-Atlantic": {"population_2020": 0, "sq_miles": 0, "sq_km": 0, "reps": 0},
+    "Southeast":    {"population_2020": 0, "sq_miles": 0, "sq_km": 0, "reps": 0},
+    "Midwest":      {"population_2020": 0, "sq_miles": 0, "sq_km": 0, "reps": 0},
+    "West":         {"population_2020": 0, "sq_miles": 0, "sq_km": 0, "reps": 0},
+}
